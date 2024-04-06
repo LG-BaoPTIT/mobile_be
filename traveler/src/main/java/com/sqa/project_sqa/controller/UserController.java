30572cd03e0f8b861fe5558d6825f75b89260713
LG-BaoPTIT <bao2002pytn@gmail.com>
2024-04-06 15:18:23 +0700

    update:bao

--- traveler/src/main/java/com/sqa/project_sqa/controller/UserController.java
+++ traveler/src/main/java/com/sqa/project_sqa/controller/UserController.java
@@ -6,12 +6,10 @@ import com.sqa.project_sqa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin()
 @RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
